8a5bd418f1ea07df8af5e038343e05090bf1adf6
Jurek <jurek2006@gmail.com>
2019-02-04 15:28:50 +0100

    Components prepared as in section 3 of the course.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,8 @@ import { HeaderComponent } from "./header/header.component";
 import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
 import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { ShoppingListComponent } from "./shopping/shopping-list/shopping-list.component";
-import { ShoppingListEditComponent } from "./shopping/shopping-list-edit/shopping-list-edit.component";
-import { ShoppingComponent } from "./shopping/shopping.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 
 @NgModule({
@@ -19,8 +18,7 @@ import { RecipesComponent } from "./recipes/recipes.component";
     RecipeItemComponent,
     RecipeDetailComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent,
-    ShoppingComponent,
+    ShoppingEditComponent,
     RecipesComponent
   ],
   imports: [BrowserModule],
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: "app-recipe-list",
@@ -6,7 +7,18 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-  recipies = [];
+  recipies: Recipe[] = [
+    new Recipe(
+      "A test recipe",
+      "This is a test",
+      "https://pinchofyum.com/wp-content/uploads/Chili-Sesame-Zoodles-Recipe.jpg"
+    ),
+    new Recipe(
+      "A test recipe",
+      "This is a test",
+      "https://pinchofyum.com/wp-content/uploads/Chili-Sesame-Zoodles-Recipe.jpg"
+    )
+  ];
 
   constructor() {}
 
