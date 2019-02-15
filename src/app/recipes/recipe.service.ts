96451f8f97462e4a706190a765bb3d1a189d4c18
Jurek <jurek2006@gmail.com>
2019-02-15 12:22:38 +0100

    113. Passing Ingredients from Recipes to the Shopping List (via a Service) - assignment start

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,22 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
-      'This is a test',
-      'https://pinchofyum.com/wp-content/uploads/Chili-Sesame-Zoodles-Recipe.jpg'
+      'Spaghetti',
+      'Bardzo smaczne spaghetti',
+      'https://www.kwestiasmaku.com/sites/kwestiasmaku.com/files/spaghetti_bolognese_01.jpg',
+      [new Ingredient('Makaron', 1), new Ingredient('Pomidor', 5)]
     ),
     new Recipe(
-      'A test 2 recipe',
-      'This is a test',
-      'https://pinchofyum.com/wp-content/uploads/Chili-Sesame-Zoodles-Recipe.jpg'
+      'Tortilla hiszpańska',
+      'Prosto z Hiszpanii',
+      'https://www.mojegotowanie.pl/media/cache/default_medium/uploads/media/default/0001/72/94c07bd4713afb2ee6c5696ed48b9505c91fc3cb.jpeg',
+      [new Ingredient('Ziemniak', 3), new Ingredient('Cebula', 5)]
     )
   ];
 
