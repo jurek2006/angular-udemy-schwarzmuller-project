71b266246bc4cf22f50e8ba75b9cfa7aae1991a8
Jurek <jurek2006@gmail.com>
2019-02-15 13:22:17 +0100

    113. Passing Ingredients from Recipes to the Shopping List (via a Service) - my solution

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { Recipe } from "../recipe.model";
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
 @Component({
-  selector: "app-recipe-detail",
-  templateUrl: "./recipe-detail.component.html",
-  styleUrls: ["./recipe-detail.component.css"]
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent {
   @Input() recipe: Recipe;
+
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  addToShoppingList() {
+    this.shoppingListService.addIngredients(this.recipe.ingredients);
+  }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -17,4 +17,25 @@ export class ShoppingListService {
     this.ingredients = [...this.ingredients, ingredient];
     this.ingredientsChanged.emit([...this.ingredients]);
   }
+
+  addIngredients(ingredients: Ingredient[]) {
+    for (const ingredient of ingredients) {
+      const foundIngredient = this.ingredients.find(
+        ing => ingredient.name === ing.name
+      );
+      if (foundIngredient) {
+        this.ingredients = [
+          ...this.ingredients.filter(ing => ing !== foundIngredient),
+          new Ingredient(
+            foundIngredient.name,
+            foundIngredient.amount + ingredient.amount
+          )
+        ];
+      } else {
+        this.ingredients = [...this.ingredients, ingredient];
+      }
+    }
+
+    console.log(this.ingredients);
+  }
 }
