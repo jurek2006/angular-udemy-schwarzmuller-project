2805dd4bfe9508afa09875264c69f18a2fb4ee59
Jurek <jurek2006@gmail.com>
2019-03-25 06:51:22 +0100

    loading recipes & shopping list on app startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { RecipeService } from "./recipes/recipe.service";
+import { ShoppingListService } from "./shopping-list/shopping-list.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(
+    private recipeService: RecipeService,
+    private shoppingListService: ShoppingListService
+  ) {}
+  ngOnInit() {
+    // loading recipes & shopping list on app startup
+    this.recipeService.loadFromServer();
+    this.shoppingListService.loadFromServer();
+  }
+}
