b2e5e6c5389966c96029c0135b0ea0f13486c00c
Jurek <jurek2006@gmail.com>
2019-03-28 07:25:20 +0100

    262. Signing Users Up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCtaqOZVrPs49fvLtBeExXbOICSCOSkNgQ",
+      authDomain: "recipe-book-project-angular.firebaseapp.com"
+    });
+  }
+}
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import * as firebase from "firebase";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthService {
+  signupUser(email: string, password: string) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(error => {
+        console.log(error);
+      });
+  }
+}
