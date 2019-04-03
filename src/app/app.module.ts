554ccd2d0aba06cf0211134dcb915b77b23e7c92
Jurek <jurek2006@gmail.com>
2019-04-03 07:15:43 +0200

    275. Creating a Recipe Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,34 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
-import { RecipesComponent } from "./recipes/recipes.component";
 import { DropdownDirective } from "./shared/dropdown.directive";
 import { AppRoutingModule } from "./app-routing.module";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { HttpModule } from "@angular/http";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { SigninComponent } from "./auth/signin/signin.component";
+import { RecipesModule } from "./recipes/recipes.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    RecipesComponent,
     DropdownDirective,
-    RecipeEditComponent,
-    RecipeStartComponent,
     SignupComponent,
     SigninComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
     FormsModule,
     AppRoutingModule,
-    HttpModule
+    HttpModule,
+    RecipesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
