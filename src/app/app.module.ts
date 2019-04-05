51b49bd32dfd9d0e806f4ccf42a67b0c51fd923e
Jurek <jurek2006@gmail.com>
2019-04-05 07:43:03 +0200

    283. Creating the Auth Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,20 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { HttpModule } from "@angular/http";
-import { SignupComponent } from "./auth/signup/signup.component";
-import { SigninComponent } from "./auth/signin/signin.component";
 import { RecipesModule } from "./recipes/recipes.module";
 import { SharedModule } from "./shared/shared.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
-import { FormsModule } from "@angular/forms";
+import { AuthModule } from "./auth/auth.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SignupComponent,
-    SigninComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule,
     SharedModule,
     RecipesModule,
     ShoppingListModule,
+    AuthModule,
     AppRoutingModule
   ],
   providers: [],
