bc75e4a8ea2d6d27de846666decbb7cbe97468f7
Jurek <jurek2006@gmail.com>
2019-04-08 07:12:54 +0200

    285. Adding Lazy Loading to the Recipes Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,17 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { HttpModule } from "@angular/http";
-import { RecipesModule } from "./recipes/recipes.module";
 import { SharedModule } from "./shared/shared.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { AuthModule } from "./auth/auth.module";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
   imports: [
     BrowserModule,
     HttpModule,
     SharedModule,
-    RecipesModule,
     ShoppingListModule,
     AuthModule,
     AppRoutingModule
--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -8,7 +8,7 @@ import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 
 const recipesRoutes: Routes = [
   {
-    path: "recipes",
+    path: "",
     component: RecipesComponent,
     children: [
       { path: "", component: RecipeStartComponent },
