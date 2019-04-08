e313483c0d721635710c1da50143527aff7acfc3
Jurek <jurek2006@gmail.com>
2019-04-08 15:18:12 +0200

    289. Creating a Basic Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,21 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { AppRoutingModule } from "./app-routing.module";
 import { HttpModule } from "@angular/http";
 import { SharedModule } from "./shared/shared.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { AuthModule } from "./auth/auth.module";
-import { HomeComponent } from "./home/home.component";
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpModule,
     SharedModule,
     ShoppingListModule,
     AuthModule,
-    AppRoutingModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
